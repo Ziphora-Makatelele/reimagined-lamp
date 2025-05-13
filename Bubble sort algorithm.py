0f9dd2e82648eb7487bb42c9a43df48a0bb2a0d7
Ziphora-Makatelele <ziphoramakatelele@gmail.com>
2025-05-13 09:59:38 +0200

    Update Bubble sort algorithm.py

--- Bubble sort algorithm.py
+++ Bubble sort algorithm.py
@@ -1,8 +1,10 @@
 my_array = [17, 23, 96, 68, 2, 75, 12, 20]
 
-n = len((my_array))
-    for i in range (n-1)
-        if my_array[j]>my_array[j+1]:
-        my_array[j],my_array[j+1]=my_array[j+1],my_array[j]
+n = len(my_array)
+
+for i in range(n-1): 
+    for j in range(n-1-i): 
+        if my_array[j] > my_array[j+1]: 
+            my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
         
 print("Sorted array: ", my_array)
